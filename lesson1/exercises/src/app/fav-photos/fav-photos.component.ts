3047ec1f06c179783c57ae799ff9010bd249a556
Josh Hilliard <125423113+jghilliard@users.noreply.github.com>
2023-03-09 15:41:44 -0600

    working

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'A Small collection of photoes.';
+  image1 = "https://cdn.pixabay.com/photo/2022/08/03/13/09/moon-7362632_1280.jpg";
+  image2 = 'https://cdn.pixabay.com/photo/2023/01/21/13/39/trees-7733877_1280.jpg';
+  image3 = 'https://cdn.pixabay.com/photo/2023/01/17/12/31/background-7724476_1280.jpg';
 
   constructor() { }
 
