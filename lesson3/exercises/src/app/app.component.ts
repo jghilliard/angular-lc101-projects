0728ee3bcdf857e6db6c8556018f3a21740fb83b
Josh Hilliard <125423113+jghilliard@users.noreply.github.com>
2023-03-12 13:40:02 -0500

    exercises!

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,65 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  ready: boolean = false;
+
+
+  handleTakeOff(){
+    let result = window.confirm("Are you sure the shuttle is ready for takeoff?");
+    if (result){
+      this.color = 'blue';
+      this.height = 10000;
+      this.width = 0;
+      this.message = "Shuttle in flight.";
+      this.ready = true;
+    }
+  }
+
+  handleLand(rocketImage){
+    let result = window.confirm("Are you sure you want to land the shuttle?");
+    if (result){
+      this.color = 'green';
+      this.height = 0;
+      this.width = 0;
+      this.message = "Shuttle has landed.";
+      this.ready = !this.ready;
+    }
+    rocketImage.style.bottom = '0px';
+  }
+
+  handleAbort(rocketImage){
+    let result = window.confirm("Are you sure you want to abort the mission");
+    if (result){
+      this.color = 'red';
+      this.height = 0;
+      this.width = 0;
+      this.message = "Mission Aborted";
+      this.ready = !this.ready;
+    }
+    rocketImage.style.bottom = '0px';
+  }
+
+  moveRocket(rocketImage, direction){
+    if (direction === "right"){
+      let movement = parseInt(rocketImage.style.left)+10+"px";
+      rocketImage.style.left = movement;
+      this.width = this.width + 10000;
+    }
+    if (direction === "left"){
+      let movement = parseInt(rocketImage.style.left)-10+"px";
+      rocketImage.style.left = movement;
+      this.width = this.width - 10000;
+    }
+    if (direction === "up"){
+      let movement = parseInt(rocketImage.style.bottom)+10+"px";
+      rocketImage.style.bottom = movement;
+      this.height = this.height + 10000;
+    }
+    if (direction === "down"){
+      let movement = parseInt(rocketImage.style.bottom)-10+"px";
+      rocketImage.style.bottom = movement;
+      this.height = this.height - 10000;
+    }
+  }
+
 }
